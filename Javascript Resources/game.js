ba5b5ff74d181392ecd2d304528d504b23a1f4fd
Akhilsudh <akhil.sudh@gmail.com>
2017-01-19 00:47:27 +0530

    Added score board --Still buggy--

--- Javascript Resources/game.js
+++ Javascript Resources/game.js
@@ -1,11 +1,14 @@
 var ball;
 var obstacles = [];
-var hitCount;
+var hitFlag;
+var score = 0;
+var endGameFlag = 0;
+
 function setup()
 {
 	createCanvas(400,600);
 	ball = new Ball();
-	obstacles.push(new Obs());
+	obstacles.push(null);
 	hitFlag = 0;
 }
 
@@ -19,29 +22,40 @@ function draw()
 
 	for(var i=0; i<obstacles.length; i++)
 	{
-		obstacles[i].show();
-		obstacles[i].update();
-		if(obstacles[i].collides(ball))
+		if(obstacles[i] != null)
 		{
-			hitFlag = 1;
-			for(var j=0; j<obstacles.length; j++)	
-			{	
-				obstacles[j].stop();
-			}
-			fill(153,76,0);
-			rect(100,250,200,100,20);
-			fill(255);
-			textSize(20);
-			text("GAME OVER!!!", 130, 305);
+				obstacles[i].show();
+				obstacles[i].update();
+				scoreDisp();
+				if(obstacles[i].collides(ball))
+				{
+					hitFlag = 1;
+					if(score != 0 && !endGameFlag)
+					{		
+						score -=1;
+						endGameFlag = 1
+					}
+					for(var j=0; j<obstacles.length; j++)	
+					{	
+						obstacles[j].stop();
+					}
+					fill(153,76,0);
+					rect(100,250,200,100,20);
+					fill(255);
+					textSize(20);
+					text("GAME OVER!!!", 130, 305);
+				}
 		}
 	}
 	
 	if(frameCount % 60 == 0 && !hitFlag)
 	{
-		if(obstacles.length==3)
+		if(obstacles.length==2)
 		{
 			obstacles.shift();
+			score += 1
 		}
+		console.log(score);
 		obstacles.push(new Obs());
 	}
 }
@@ -58,4 +72,13 @@ function keyTyped()
 	{
 		console.log(obstacles.length);
 	}
+}
+
+function scoreDisp()
+{
+	fill(153,76,0);
+	rect(340,20,50,50,20);
+	fill(255);
+	textSize(20);
+	text(score, 350, 55);
 }
